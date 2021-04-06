0ffd93e2676e56d53cabf22a7d8a9391ea7f3bb0
Jason Nordheim <jason.nordheim@gmail.com>
2021-04-06 13:02:38 -0600

    added first problem/solution

---
+++ removeDuplicates.js
@@ -0,0 +1,21 @@
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * Constraints
+ * - `0 <= nums.length <= 3 * 10⁴`
+ * - `-10⁴ <= nums[i] <= 10⁴`
+ * - `nums` is sorted in ascending order.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
